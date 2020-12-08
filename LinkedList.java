b79c0f0196d740875de0ccba849908ef9e8a94d3
VedantaMaha <vedanta.maha@zettabyte.sg>
2020-12-09 01:11:12 +0800

    add remove and indexof function

--- LinkedList.java
+++ LinkedList.java
@@ -31,6 +31,46 @@ class LinkedList {
         current.next = new Node(data);
     }
 
+    public void remove(int data) {
+        if (head == null) {
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public int indexOf(int data) {
+        if (head == null) {
+            return -1;
+        }
+        if (head.data == data) {
+            return 0;
+        }
+        int index = 0;
+        int foundIndex = -1;
+        Node current = head;
+        while (current.next != null) {
+            // start searching from index 1 to last index
+            current = current.next;
+            index += 1;
+            if (current.data == data) {
+                foundIndex = index;
+                break;
+            }
+        }
+        return foundIndex;
+    }
+
     public void print() {
         if (head == null) {
             System.out.println("Linked list is empty.");
@@ -53,9 +93,13 @@ class LinkedList {
 class Main {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 27; i < 38; i++) {
         //    ll.prepend(i);
+           ll.append(i);
         }
         ll.print();
+        // ll.remove(5);
+        int seachItem = 32;
+        System.out.printf("data %d is in index %d\n", seachItem, ll.indexOf(seachItem));
     }
 }
